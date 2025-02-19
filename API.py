855cd14be239179e8a43e1a7ab6b150585d127d7
Vivek-S-Patel <vivekp0418@gmail.com>
2025-02-19 17:48:08 -0500

    Update API.py
    added to modules

--- API.py
+++ API.py
@@ -2,16 +2,17 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# entities
+# Dummy database
 users = []
 houses = []
 rooms = []
 devices = []
 
-# Users
+# User Routes
 @app.route('/users', methods=['POST'])
 def create_user():
     username = request.json.get('username')
+    house_id = request.json.get('house_id')
     return ""
 
 @app.route('/users', methods=['GET'])
@@ -21,16 +22,18 @@ def get_users():
 @app.route('/users/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
     username = request.json.get('username')
+    house_id = request.json.get('house_id')
     return ""
 
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
     return ""
 
-# Houses
+# House Routes
 @app.route('/houses', methods=['POST'])
 def create_house():
     username = request.json.get('username')
+    num_rooms = request.json.get('num_rooms')
     return ""
 
 @app.route('/houses', methods=['GET'])
@@ -40,16 +43,18 @@ def get_houses():
 @app.route('/houses/<int:house_id>', methods=['PUT'])
 def update_house(house_id):
     username = request.json.get('username')
+    num_rooms = request.json.get('num_rooms')
     return ""
 
 @app.route('/houses/<int:house_id>', methods=['DELETE'])
 def delete_house(house_id):
     return ""
 
-# Rooms
+# Room Routes
 @app.route('/rooms', methods=['POST'])
 def create_room():
     username = request.json.get('username')
+    room_name = request.json.get('room_name')
     return ""
 
 @app.route('/rooms', methods=['GET'])
@@ -59,13 +64,14 @@ def get_rooms():
 @app.route('/rooms/<int:room_id>', methods=['PUT'])
 def update_room(room_id):
     username = request.json.get('username')
+    room_name = request.json.get('room_name')
     return ""
 
 @app.route('/rooms/<int:room_id>', methods=['DELETE'])
 def delete_room(room_id):
     return ""
 
-# Devices
+# Device Routes
 @app.route('/devices', methods=['POST'])
 def create_device():
     username = request.json.get('username')
